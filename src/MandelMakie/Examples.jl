102f88571d453a232a3d284845425a9b52e33a16
Forrest Hilton <forrestmhilton@gmail.com>
2024-09-26 14:07:39 -0500

    rays show but only update when view is swaped

--- src/MandelMakie/Examples.jl
+++ src/MandelMakie/Examples.jl
@@ -38,10 +38,10 @@ First, you define a family of complex maps
 """
 
 # ╔═╡ f49a6cef-86c9-4a47-a9ff-30588b3de27c
-f(z, λ) = z^2 + λ / z^2
+f(z, c) = z^2 + c
 
 # ╔═╡ 89426560-0874-45a2-a2dd-6b2a044faabb
-crit(λ) = λ^(1 / 4)
+crit(c) = c
 
 # ╔═╡ 6b1c8547-06de-4bb7-9062-0826ad04b92e
 md"""
@@ -49,10 +49,7 @@ Then, call `Viewer` or `Viewer3D` to open a window where you see the Mandelbrot 
 """
 
 # ╔═╡ 70f82c8f-5b1b-46c7-87b3-5fad91ba1094
-# ╠═╡ disabled = true
-#=╠═╡
 Viewer(f; crit = crit, mandel_diameter = 1.0)
-  ╠═╡ =#
 
 # ╔═╡ 048320b4-12f2-4e7f-b267-8b15ad2485e0
 # ╠═╡ disabled = true
--- src/MandelMakie/src/Refactor.jl
+++ src/MandelMakie/src/Refactor.jl
@@ -3,6 +3,7 @@ module Refactor
 export Viewer, Attractor, get_attractors, get_parameter, critical_points
 
 using GLMakie, Symbolics, StaticArraysCore, LinearAlgebra, Polynomials, HypertextLiteral
+include("./Rays.jl")
 using GLMakie.Colors
 using GLMakie.Colors: LCHab
 using Crayons
@@ -524,6 +525,10 @@ function get_attractors(f::Function; projective::Bool = false, ε::Real = 1e-4)
 end
 
 function get_attractors(f::Function, c::Number; projective::Bool = false, ε::Real = 1e-4)
+    return find_attractors(h, 0.0im; projective = projective)
+end
+
+function find_attractors(f::Function, c::Number; projective::Bool = false)
     # If it is not a family ignores the parameter
     hasmethod(f, Tuple{ComplexF64,ComplexF64}) ||
         return get_attractors(f, projective = projective, ε = ε)
@@ -684,6 +689,7 @@ mutable struct MandelView <: View
     colors::Observable{Matrix{RGBA{Float64}}}
     points::Observable{Vector{ComplexF64}}
     marks::Observable{Vector{ComplexF64}}
+    rays::Observable{Vector{Vector{ComplexF64}}}
 
     coloring_data::ColoringData
 
@@ -694,6 +700,7 @@ mutable struct MandelView <: View
         colors = zeros(RGBA{Float64}, pixels, pixels)
         points = ComplexF64[center]
         marks = ComplexF64[]
+        rays = Vector{ComplexF64}[]
 
         return new(
             center,
@@ -704,6 +711,7 @@ mutable struct MandelView <: View
             colors,
             points,
             marks,
+            rays,
             coloring_data,
         )
     end
@@ -721,6 +729,7 @@ mutable struct JuliaView <: View
     colors::Observable{Matrix{RGBA{Float64}}}
     points::Observable{Vector{ComplexF64}}
     marks::Observable{Vector{ComplexF64}}
+    rays::Observable{Vector{Vector{ComplexF64}}}
 
     coloring_data::ColoringData
 
@@ -731,6 +740,7 @@ mutable struct JuliaView <: View
         colors = zeros(RGBA{Float64}, pixels, pixels)
         points = ComplexF64[center]
         marks = ComplexF64[]
+        rays = Vector{ComplexF64}[]
 
         return new(
             center,
@@ -742,6 +752,7 @@ mutable struct JuliaView <: View
             colors,
             points,
             marks,
+            rays,
             coloring_data,
         )
     end
@@ -909,6 +920,7 @@ function update_view!(view::View, d_system::DynamicalSystem, options::Options)
     notify(view.colors)
     notify(view.points)
     notify(view.marks)
+    notify(view.rays)
 
     return view
 end
@@ -920,6 +932,7 @@ function pick_parameter!(
     options::Options,
     point,
 )
+    println("here")
     julia.parameter = point
 
     julia.marks[] = orbit(
@@ -928,6 +941,7 @@ function pick_parameter!(
         julia.parameter,
         options.critical_length - 1,
     )
+    julia.rays[] = Rays.rays([julia.parameter, 0, 1])
 
     mandel.points[] = [julia.parameter]
 
@@ -1060,6 +1074,15 @@ function create_plot!(frame::Frame)
     end
 
     lines!(frame.axis, mark_vectors, color = (:blue, 0.5), inspectable = false)
+
+    for ray in view.rays[]
+        ray_vectors = lift(ray) do zs
+            xs, ys = to_pixel_space(view, zs)
+            return Point2f.(xs, ys)
+        end
+
+        lines!(frame.axis, ray_vectors, color = (:yellow, 0.5), inspectable = false)
+    end
 
     scatter!(
         frame.axis,
@@ -1539,6 +1562,7 @@ struct Viewer
         )
 
         julia.marks[] = [d_system.critical_point(julia.parameter)]
+        julia.rays[] = Rays.rays([julia.parameter, 0, 1])
         pick_orbit!(julia, d_system, options, julia.points[][begin])
 
         left_frame, right_frame = create_frames!(figure, options, mandel, julia)
