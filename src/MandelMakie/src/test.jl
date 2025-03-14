d5db08df5da1c4c070cdcc45a38438c0610d1ae5
Forrest Hilton <forrestmhilton@gmail.com>
2025-03-13 23:00:44 -0500

    Vectorize newtons method

--- src/MandelMakie/src/test.jl
+++ src/MandelMakie/src/test.jl
@@ -17,30 +17,68 @@ ex2 = symbolic_iterate(cpts[2], m)
 
 eq = ex1 ~ ex2
 
-function solve_newton(eq, x, x₀; abstol = 1e-8, maxiters = 50)
-    # symbolic expressions for f(x) and f′(x)
-    f = eq.lhs - eq.rhs # want to find root of f(x)
-    f′ = Symbolics.derivative(f, x)
+function symbolic_to_function(f)
+    txt = string(build_function(f, c))
+    code = replace(txt, "NaNMath." => "")
+    lines = split(code, '\n')
+    lines[1] = "function (c::Vector{ComplexF64})"
+    lines[5] = "    return @. " * lstrip(lines[5])
+    new_code = """"""
+    for line in lines
+        if startswith(lstrip(line), "#")
+            continue
+        end
+        new_code = new_code * line * "\n"
+    end
+    # println(new_code)
+    return eval(Meta.parse(new_code))
+end
 
-    xₙ = x₀ # numerical value of the initial guess
-    for i in 1:maxiters
-        # calculate new guess by numerically evaluating symbolic expression at previous guess
-        xₙ₊₁ = substitute(x - f / f′, x => xₙ)
-        if abs(xₙ₊₁ - xₙ) < abstol
-            return xₙ₊₁ # converged
+struct NewtonSolver
+    eq::Any
+    x::Any
+    f::Any
+    f′::Any
+    ffunc::Any
+    fpfunc::Any
+    abstol::Float64
+    maxiters::Int
+
+    function NewtonSolver(eq, x; abstol = 1e-8, maxiters = 50)
+        f = eq.lhs - eq.rhs
+        ffunc = symbolic_to_function(f)
+        f′ = Symbolics.derivative(f, x)
+        fpfunc = symbolic_to_function(f′)
+
+        new(eq, x, f, f′, ffunc, fpfunc, abstol, maxiters)
+    end
+end
+
+function solve(solver, x₀s)
+    x₀s = collect(x₀s)
+    xₙs = copy(x₀s)
+
+    for _ in 1:solver.maxiters
+        xₙ₊₁s =
+            Complex(1) .-
+            Base.invokelatest(solver.ffunc, xₙs) ./ Base.invokelatest(solver.fpfunc, xₙs)
+
+        converged = abs.(xₙ₊₁s .- xₙs) .< solver.abstol
+        if all(converged)
+            return xₙ₊₁s
         else
-            xₙ = xₙ₊₁
+            xₙs[.!converged] .= xₙ₊₁s[.!converged]
         end
     end
-    error("Newton's method failed to converge")
+    error("Newton's method failed to converge for some initial guesses")
 end
 
-function print_many()
-    for i in -50:50
-        println(solve_newton(eq, c, Complex(0.5 + (i / 25 * 1im))))
-    end
-end
-print_many()
+solver = NewtonSolver(eq, c)
+initial_guesses = [Complex(0.5 + (i / 25 * 1im)) for i in -50:50]
+println(solve(solver, initial_guesses[1:2]))
+results = @time solve(solver, initial_guesses)
+
+println.(results)
 
 # print(solve(ex1-ex2,c))
 # Sym{PythonCall.Core.Py}[-1.73205080756888, 1.73205080756888, -1.85790551759459*I, 1.8579055175
